333e4fd8c7ec1ffae128f48985fdf7fdb6a75c95
Shijin Suresh V <shijinvs@gmail.com>
2016-07-26 12:51:43 +0530

    Finish user edit,update,index and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,24 +3,13 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-<<<<<<< HEAD
-  include ApplicationHelper
-  
 
   # Returns true if a test user is logged in.
-=======
-   # Returns true if a test user is logged in.
->>>>>>> advanced-login
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-<<<<<<< HEAD
-
-  # Add more helper methods to be used by all tests here...
-=======
   # Log in as a particular user.
   def log_in_as(user)
     session[:user_id] = user.id
@@ -35,5 +24,4 @@ class ActionDispatch::IntegrationTest
                                           password: password,
                                           remember_me: remember_me } }
   end
->>>>>>> advanced-login
 end
